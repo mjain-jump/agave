2a8678d9bec8ff7e7e227034826492d8dfbb44ac
Manik Jain <mjain@jumptrading.com>
2026-02-05 21:58:37 +0000

    cpi: truncate CpiError log messages

--- program-runtime/src/cpi.rs
+++ program-runtime/src/cpi.rs
@@ -32,26 +32,23 @@ pub enum CpiError {
     InvalidPointer,
     #[error("Too many signers")]
     TooManySigners,
-    #[error("Could not create program address with signer seeds: {0}")]
+    #[error("Could not create program address with signer seeds")]
     BadSeeds(PubkeyError),
     #[error("InvalidLength")]
     InvalidLength,
-    #[error("Invoked an instruction with too many accounts ({num_accounts} > {max_accounts})")]
+    #[error("Invoked an instruction with too many accounts")]
     MaxInstructionAccountsExceeded {
         num_accounts: u64,
         max_accounts: u64,
     },
     #[error("Invoked an instruction with data that is too large ({data_len} > {max_data_len})")]
     MaxInstructionDataLenExceeded { data_len: u64, max_data_len: u64 },
-    #[error(
-        "Invoked an instruction with too many account info's ({num_account_infos} > \
-         {max_account_infos})"
-    )]
+    #[error("Invoked an instruction with too many account info's")]
     MaxInstructionAccountInfosExceeded {
         num_account_infos: u64,
         max_account_infos: u64,
     },
-    #[error("Program {0} not supported by inner instructions")]
+    #[error("Program not supported by inner instructions")]
     ProgramNotSupported(Pubkey),
 }
 
